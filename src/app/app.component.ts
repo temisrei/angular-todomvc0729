784ea52a5fbabb487dd13e5257e82c17745adbd0
Rei Lin <temisrei@gmail.com>
2017-07-30 12:10:08 +0800

    套用屬性指令 (Attribute Directives) 作法二

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,12 @@ export class AppComponent {
 
   addTodo() {
     if (this.todo) {
-      this.todos.push(this.todo);
+      this.todos.push({
+        text: this.todo,
+        done: false
+      });
       this.todo = '';
     }
   }
+
 }
